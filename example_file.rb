5e5ff8f1ee0e434b28d53d5e61677ea0493c5afa
Shevaughn Grant <gshevaughn@gmail.com>
2020-10-05 22:26:07 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,26 @@
+puts "Hello World"
+
+name = "Shevaughn"
+
+if name == "Shevaughn"
+  puts "Hello Shevaughn"
+elsif name == "Javaughn"
+  puts "Don't be late fish boy"
+elsif name == "Renee"
+  puts "Welcome home"
+elsif name == "Ariyah"
+  puts "Playtime is over"
+else
+  puts "Who are you ?"
+end
+
+current_weather = "raining"
+ 
+case current_weather
+  when "sunny"
+    puts "grab some sunscreen!"
+  when "raining"
+    puts "grab an umbrella"
+  when "snowing"
+    puts "bundle up"
+end
---
+++ learning-comments.rb
@@ -0,0 +1,7 @@
+# Display message
+puts "Hello World"
+
+puts "Joy to the world"
+puts "All the boys and girls"
+# puts "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
